d423676bc6e85fddcdba898bb9a819288cb011f0
Mark J. Pickering <mark.pickering@capgemini.com>
2023-12-14 11:51:26 +0000

    Project structure changes

---
+++ CSharp.Tests/Unit/GuildedRose/UpdateQualityTests.cs
@@ -0,0 +1,28 @@
+﻿using csharp;
+
+namespace CSharp.Tests.Unit.GuildedRose;
+
+public class UpdateQualityTests
+{
+    [Fact]
+    public void WhenBeforeSellByDate_DecrementQualityBy1()
+    {
+        // Arrange
+        var Items = new List<Item> { new Item { Name = "foo", SellIn = 3, Quality = 10 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+
+        Items.First().Quality.Should().Be(9);
+    }
+
+    [Fact]
+    public void WhenAfterSellByDate_DecrementQualityBy2()
+    {
+        // Arrange
+        var Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 8 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+
+        Items.First().Quality.Should().Be(8);
+    }
+}
